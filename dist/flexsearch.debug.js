bd0d71d40256a47808f6878cab27d13c0375f447
Andrew Lee <andrew.lee@nightwatchapp.com>
2021-08-10 20:14:11 -0400

    chore(export): build

--- dist/flexsearch.debug.js
+++ dist/flexsearch.debug.js
@@ -392,7 +392,7 @@ function F(a) {
 }
 ;var ia = {encode:ha, rtl:!1, tokenize:""};
 function ha(a) {
-  return ca.call(this, a.toLowerCase(), !1, da, !1);
+  return ca.call(this, ("" + a).toLowerCase(), !1, da, !1);
 }
 ;const ja = {}, H = {};
 function ka(a) {
@@ -1192,7 +1192,7 @@ R.prototype.export = function(a, b, c, d, e) {
         k = f.store;
         break;
       default:
-        return Promise.resolve(!0);
+        return !0;
     }
     return yield qa(a, f, g, d, e, k);
   });
