858d6952188096892e589adb5029b8c58d791078
Matthew Rathbone <matthew.rathbone@gmail.com>
2021-09-03 21:20:52 -0500

    reverse node check test

--- src/worker/index.js
+++ src/worker/index.js
@@ -39,8 +39,8 @@ function WorkerIndex(options){
 
         factory = factory.toString();
     }
-
-    const is_node_js = self["exports"];
+    const is_browser = typeof window !== 'undefined' && typeof window.document !== 'undefined';
+    const is_node_js = !is_browser;
     const _self = this;
 
     this.worker = create(factory, is_node_js, options["worker"]);
