8588c50c46fe36faf552673bd0fadb780a41ce44
Thomas Wilkerling <thomas@nextapps.de>
2025-04-13 15:49:08 +0200

    add missing charset to sources

---
+++ src/charset/cjk.js
@@ -0,0 +1,14 @@
+import { EncoderOptions } from "../type.js";
+
+// https://en.wikipedia.org/wiki/CJK_characters
+
+/** @type EncoderOptions */
+const options = {
+    split: ""
+    //normalize: true
+    // normalize: function(str){
+    //     return str.toLowerCase();
+    // },
+    //dedupe: false
+};
+export default options;
